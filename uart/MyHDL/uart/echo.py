351e71532317ead442036a6000fdefd8c5207b1f
Edward Vidal Jr <develone@sbcglobal.net>
2018-08-03 10:24:10 +0000

    adding the start of add small memory to design documenting FSMs for send & recv

--- uart/MyHDL/uart/echo.py
+++ uart/MyHDL/uart/echo.py
@@ -3,6 +3,7 @@ from myhdl import block, always, Signal, instance, delay, Simulation, intbv, enu
 from uart_rx import uart_rx, t_state_rx
 from uart_tx import uart_tx, t_state_tx
 from forceHi import forceHi
+from ram import ram
 
 i_Clk  = Signal(bool(0))
 w_TX_Serial = Signal(bool(0))
@@ -17,7 +18,11 @@ i_uart_rx = Signal(bool(0))
 o_uart_tx = Signal(bool(0))
 o_TX_Done = Signal(bool(0))
 
-
+dout = Signal(intbv(0)[16:])
+dout_v = Signal(intbv(0)[15:])
+din = Signal(intbv(0)[15:])
+addr = Signal(intbv(0)[7:])
+we = Signal(bool(0))
 
 @block	
 def echo(i_Clk, i_uart_rx, o_uart_tx,CLKS_PER_BIT=None):
@@ -26,12 +31,13 @@ def echo(i_Clk, i_uart_rx, o_uart_tx,CLKS_PER_BIT=None):
 	uart_rx_0 = uart_rx(i_Clk,i_uart_rx,w_RX_DV,w_RX_Byte,state_rx,CLKS_PER_BIT=868)
 	uart_tx_0 = uart_tx(i_Clk,w_RX_DV,w_RX_Byte,w_TX_Active,w_TX_Serial,o_TX_Done,state_tx,CLKS_PER_BIT=868)
 	forceHi_0 = forceHi(w_TX_Serial,w_TX_Active,o_uart_tx)
+	ram_0 = ram(dout, din, addr, we, i_Clk, depth=32)
  
 	
  
 	 
  
-	return uart_tx_0, uart_rx_0, forceHi_0 
+	return uart_tx_0, uart_rx_0, forceHi_0, ram_0
 
 def convert_echo(hdl):
 	"""Convert inc block to Verilog or VHDL."""
--- uart/MyHDL/uart/trace_tx.py
+++ uart/MyHDL/uart/trace_tx.py
@@ -1,8 +1,9 @@
 import myhdl
 from myhdl import block, always, instance, Signal, ResetSignal, intbv, delay,StopSimulation
 from uart_tx import uart_tx, t_state_tx
-from uart_rx import uart_rx, t_state_rx
+#from uart_rx import uart_rx, t_state_rx
 from forceHi import forceHi
+from ram import ram
 
 ACTIVE_LOW = 0
 @block
@@ -36,17 +37,51 @@ def testbench():
 	w_RX_DV = Signal(bool(0))
 	w_RX_Byte = Signal(intbv(0)[8:])
 	state_tx = Signal(t_state_tx.TX_IDLE)
-	state_rx = Signal(t_state_rx.TX_IDLE) 
+	#state_rx = Signal(t_state_rx.TX_IDLE)
+	
+	dout = Signal(intbv(0)[16:])
+	dout_v = Signal(intbv(0)[16:])
+	din = Signal(intbv(0)[16:])
+	addr = Signal(intbv(0)[7:])
+	we = Signal(bool(0))
+	#i_Clock = Signal(bool(0)) 
 	msg = ['h','e','l','l','o','w','o','l','d']
-	uart_rx(i_Clk,i_uart_rx,w_RX_DV,w_RX_Byte,state_rx,CLKS_PER_BIT=868)
+	#uart_rx(i_Clk,i_uart_rx,w_RX_DV,w_RX_Byte,state_rx,CLKS_PER_BIT=868)
 	uart_tx_0 = uart_tx(i_Clk,w_RX_DV,w_RX_Byte,w_TX_Active,w_TX_Serial,o_TX_Done,state_tx,CLKS_PER_BIT=868)
 	
 	forceHi_0 = forceHi(w_TX_Serial,w_TX_Active,o_uart_tx)
+	ram_0 = ram(dout, din, addr, we, i_Clk, depth=32)
+	
 	@always(delay(10))
 	def clkgen():
 		i_Clk.next = not i_Clk
 	@instance
 	def stimulus():
+		addr.next = 0
+		din.next = 0
+		we.next = 1
+
+		for i in range(1):
+			yield i_Clk.posedge			
+		for i in range(30): 
+			din.next = din
+			addr.next = addr + 1
+			din.next = din + 1	
+			yield i_Clk.posedge
+		we.next = 0		
+		for i in range(1):
+			yield i_Clk.posedge
+				
+		addr.next = 0
+		#din.next = 0
+		#we.next = 1
+		for i in range(1):
+			yield i_Clk.posedge
+		for i in range(30): 
+			#din.next = din
+			addr.next = addr + 1
+			#din.next = din + 1	
+			yield i_Clk.posedge					
 		for j in range(8):
 			for i in range(100):
 				yield i_Clk.posedge 
@@ -67,9 +102,10 @@ def testbench():
 			for i in range(9080):
 				yield i_Clk.posedge
 			for i in range(100):
-				yield i_Clk.posedge 				
+				yield i_Clk.posedge
+			
  		raise StopSimulation()
-	return uart_tx_0, forceHi_0, clkgen, stimulus
+	return uart_tx_0, forceHi_0, ram_0, clkgen, stimulus
 tb = testbench()
 tb.config_sim(trace=True)
 tb.run_sim()
