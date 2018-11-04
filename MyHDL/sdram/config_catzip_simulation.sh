9946ceeb4e618ddd6bade8350e80283bb9e246eb
Edward Vidal Jr <develone@sbcglobal.net>
2018-11-04 15:39:25 -0700

    continued work on why the simulation fails

--- MyHDL/sdram/config_catzip_simulation.sh
+++ MyHDL/sdram/config_catzip_simulation.sh
@@ -3,9 +3,11 @@
 #modifications 
 # Makefile_autodata
 #new files
-#sdramscope.txt wbscope.v sdramscope.cpp scopecls.cpp scopecls.h
-#buildsdramscope.sh
-
+#sdramscope.txt  sdramscope.cpp scopecls.cpp scopecls.h
+#buildsdramscope.sh clockpll50,txt
+#o_ram_ce_n	--> o_ram_cs_n
+#o_ram_oe_n --> o_ram_ras_n
+#o_ram_sel --> o_ram_cas_n
 cp ~/testbuilds/learning_hdl/MyHDL/sdram/Makefile_autodata \
 ~/testbuilds/catzip_simulation/catzip/auto-data/Makefile
 
@@ -18,11 +20,6 @@ cp ~/testbuilds/learning_hdl/MyHDL/sdram/sdramscope.txt \
 diff ~/testbuilds/learning_hdl/MyHDL/sdram/sdramscope.txt \
 ~/testbuilds/catzip_simulation/catzip/auto-data/sdramscope.txt
 
-cp ~/testbuilds/learning_hdl/MyHDL/sdram/wbscope.v \
-~/testbuilds/catzip_simulation/catzip/rtl/catzip
-
-diff ~/testbuilds/learning_hdl/MyHDL/sdram/wbscope.v \
-~/testbuilds/catzip_simulation/catzip/rtl/catzip/wbscope.v
 
 cp ~/testbuilds/learning_hdl/MyHDL/sdram/sdramscope.cpp \
 ~/testbuilds/catzip_simulation/catzip/sw/host
@@ -55,7 +52,17 @@ cp ~/testbuilds/learning_hdl/MyHDL/sdram/buildsdramscope.sh \
 diff ~/testbuilds/learning_hdl/MyHDL/sdram/buildsdramscope.sh \
 ~/testbuilds/catzip_simulation/catzip/sw/host/buildsdramscope.sh
 
+cp ~/testbuilds/learning_hdl/MyHDL/sdram/clockpll50.txt \
+~/testbuilds/catzip_simulation/catzip/auto-data/
 
+diff ~/testbuilds/learning_hdl/MyHDL/sdram/clockpll50.txt \
+~/testbuilds/catzip_simulation/catzip/auto-data/clockpll50.txt
+
+#cp ~/testbuilds/learning_hdl/MyHDL/sdram/wbscope.v \
+#~/testbuilds/catzip_simulation/catzip/rtl/catzip
+
+#diff ~/testbuilds/learning_hdl/MyHDL/sdram/wbscope.v \
+#~/testbuilds/catzip_simulation/catzip/rtl/catzip/wbscope.v
 
 
 
